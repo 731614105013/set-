ef71ab21c86a6e62f85702cec2e475b4828df202
731614105013 <gomathiammupk29@gmail.com>
2017-07-05 15:08:36 +0530

    Create Oddno.java

---
+++ Oddno.java
@@ -0,0 +1,24 @@
+# set-
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Oddno
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int u;
+    for(int j=0;j<n;j++){
+      if(j%2!=0)
+      {
+        System.out.println("The numbers are:"+j);
+        
+      }
+      
+    }
+  }
+}
+      
+      
+    
